871751ae0e34de280add5dc60db71eed00dab0e7
ben <grzybowski1911@gmail.com>
2022-07-02 20:12:37 -0700

    scroll up function fade logo functionality

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+const fadeLogo = () => {
+    var pos = window.scrollY;
+    var hero = document.getElementById('hero').getBoundingClientRect().height;
+    var logo = document.getElementById('banner-logo');
+    if(pos <= hero) {
+        var op = 1 - pos / hero; 
+        } else {
+            op = 1;
+        }
+    logo.style.opacity = op;
+}
+window.addEventListener('scroll', fadeLogo);
